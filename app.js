cb5a3afc087d5412ae7a0ee0da832aaf98288b48
Eunju/kiori <eun01300714@gmail.com>
2019-08-19 19:26:35 +0900

    #3 make a room

---
+++ app.js
@@ -0,0 +1,52 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
+let room = ['room1', 'room2'];
+let roomNum = 0;
+
+
+app.get('/', (req, res) => {
+  res.render('chat');
+});
+
+
+io.on('connection', (socket) => {
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+
+  socket.on('leaveRoom', (num, name) => {
+    socket.leave(room[num], () => {
+      console.log(name + ' leave a ' + room[num]);
+      console.log('leave '+num);
+      io.to(room[num]).emit('leaveRoom', num, name);
+    });
+  });
+
+
+  socket.on('joinRoom', (num, name) => {
+    socket.join(room[num], () => {
+      console.log(name + ' join a ' + room[num]);
+      console.log('join '+num);
+      io.to(room[num]).emit('joinRoom', num, name);
+    });
+  });
+
+
+  socket.on('chat message', (num, name, msg) => {
+    roomNum = num;
+    io.to(room[roomNum]).emit('chat message', name, msg);
+  });
+});
+
+
+http.listen(3000, () => {
+  console.log('Connect at 3000');
+});
--- main.js
+++ main.js
@@ -2,8 +2,17 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+// app.set('view engine', 'ejs');
+// app.set('views', './views');
+
+let roomNum = 0;
+
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/main.html');
+});
+
+app.post('/main', function(req, res) {
+    // res.sendfile(__dirname + '/main.html');
 });
 
 io.on('connection', function(socket){
@@ -11,15 +20,30 @@ io.on('connection', function(socket){
     socket.on('disconnect', function(){
         console.log('user disconnected');
     });
-    socket.broadcast.emit('hi');
+    // socket.broadcast.emit('hi');
 
-    socket.on('chat message', function(data){
-        io.emit('chat message', data.msg);
+    socket.on('leaveRoom', (num, name)=> {
+        socket.leave(num, ()=> {
+            console.log(name + ' leave a room ' + num);
+            io.to(num).emit('leaveRoom', num, name);
+        });
+    });
+    
+    socket.on('joinRoom', (num, name)=> {
+        socket.join(num, () => {
+            console.log(name + ' join a room ' + num);
+            io.to(num).emit('joinRoom', num, name);
+        });
+    });
+
+    socket.on('chat message', (num, name, msg) => {
+        roomNum = num;
+        io.to(roomNum).emit('chat message', name, msg);
     });
 });
 
 http.listen(3000, function(){
-    console.log('listening on *:3000');
+    console.log('success');
 });
 
 io.emit('some event', { for: 'everyone' });
