599c0e97347e224e3b1a92f757e767d1c4b37f91
Eunju/kiori <eun01300714@gmail.com>
2019-08-21 02:03:57 +0900

    #7 make form by pug

---
+++ js/gameChat.js
@@ -0,0 +1,29 @@
+$(() => {
+    console.log('connected');
+    // const name = prompt('What your name');
+    // const Roomcode = prompt('Write room code or create new room code');
+    const socket = io();
+
+    socket.emit('joinRoom', Roomcode, name);
+
+    $('form').submit(() => {
+      socket.emit('chat message', Roomcode, name, $('#m').val());
+      $('#m').val('');
+      return false;
+    });
+
+    socket.on('chat message', (name, msg) => {
+      $('#messages').append($('<li>').text(name + '  :  ' +
+        msg));
+    });
+
+    socket.on('leaveRoom', (Roomcode, name) => {
+      $('#messages').append($('<li>').text(name + '    leaved '
+        + Roomcode + ' :('));
+    });
+
+    socket.on('joinRoom', (Roomcode, name) => {
+      $('#messages').append($('<li>').text(name + '    joined '
+        +Roomcode + ':)'));
+    });
+  });
--- main.js
+++ main.js
@@ -1,25 +1,65 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+var bodyParser = require('body-parser');
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/main.html');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+var rooms = [];
+var name, room;
+
+app.get('/', (req, res) => {
+  // res.render('main');
+  // res.sendFile(__dirname+'/views/index.html');
+  res.render('enter');
 });
 
-io.on('connection', function(socket){
-    console.log('a user connected');
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
+app.post('/enterProc', (req, res) => {
+  res.render('main', {
+    name : req.body.name,
+    Roomcode : req.body.room
+  });
+  // res.redirect('/gameChat');
+});
+
+app.get('/gameChat', (req, res) => {
+
+});
+
+io.on('connection', (socket) => {
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('leaveRoom', (Roomcode, name) => {
+    socket.leave(Roomcode, () => {
+      console.log(name + ' leave a ' + Roomcode);
+      io.to(Roomcode).emit('leaveRoom', Roomcode, name);
     });
-    socket.broadcast.emit('hi');
+  });
 
-    socket.on('chat message', function(data){
-        io.emit('chat message', data.msg);
+
+  socket.on('joinRoom', (Roomcode, name) => {
+    socket.join(Roomcode, () => {
+      console.log(name + ' join a ' + Roomcode);
+      if(!rooms.find(c => c===Roomcode))
+        rooms.push(Roomcode);
+      console.log(rooms);
+      io.to(Roomcode).emit('joinRoom', Roomcode, name);
     });
+  });
+
+
+  socket.on('chat message', (Roomcode, name, msg) => {
+    // insert msg to db
+    io.to(Roomcode).emit('chat message', name, msg);
+  });
 });
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
-});
 
-io.emit('some event', { for: 'everyone' });
+http.listen(3000, () => {
+  console.log('Connect at 3000');
+});
