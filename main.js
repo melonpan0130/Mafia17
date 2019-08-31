174e9b11a971f1bfb91b4a2ca5b15ff0d057a6ae
Eunju/kiori <eun01300714@gmail.com>
2019-08-31 22:18:33 +0900

    #3 When someone comes in, add a profile.

--- main.js
+++ main.js
@@ -14,14 +14,15 @@ var db = mysql.createConnection({
 
 db.connect();
 
-
-
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.locals.pretty = true;
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 
 var rooms = [];
+
 
 app.get('/', (req, res) => {
   /*
@@ -40,8 +41,8 @@ app.get('/img/job/:filename', (req, res) => {
   fs.readFile('img/job/'+req.params.filename, (error, data) => {
     res.writeHead(200, { 'Content-Type': 'image/png'});
     res.end(data);
-  })
-})
+  });
+});
 
 io.on('connection', (socket) => {
   console.log('connection');
@@ -49,22 +50,55 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     // db.end();
     console.log('user disconnected');
+
   });
 
-  socket.on('leaveRoom', (Roomcode, name) => {
+  socket.on('leave', (Roomcode, name) => {
     socket.leave(Roomcode, () => {
-      console.log(name + ' leave a ' + Roomcode);
-      io.to(Roomcode).emit('leaveRoom', Roomcode, name);
+      db.query('DELETE FROM player WHERE roomcode = ? AND name = ?'
+      , [Roomcode, name]
+      , () => {
+        console.log(name + ' leave a ' + Roomcode);
+        io.to(Roomcode).emit('leaveRoom', Roomcode, name);
+      });
     });
   });
 
   socket.on('joinRoom', (Roomcode, name) => {
     socket.join(Roomcode, () => {
-      console.log(name + ' join a ' + Roomcode);
-      if(!rooms.find(c => c===Roomcode))
-        rooms.push(Roomcode);
-      console.log(rooms);
-      io.to(Roomcode).emit('joinRoom', Roomcode, name);
+      
+      db.query('INSERT INTO player VALUES( ?, ? )'
+      , [Roomcode, name]
+      , ()=> {
+        db.query('SELECT * FROM player WHERE roomcode = ?'
+        , [Roomcode]
+        , function(err, players) {
+          console.log(name + ' join a ' + Roomcode);
+          console.log(players);
+
+          io.to(Roomcode).emit('joined', Roomcode, name, players);
+        });
+
+        // RoomCode가 새로운 방인가?
+        /*
+        var isNewRoom = rooms.filter((room)=>{return room.room == Roomcode});
+        console.log(isNewRoom);
+        if(!isNewRoom)
+          rooms.push({ room : Roomcode, member : [name]});
+        else
+          isNewRoom.member.push(name);
+        console.log(rooms);
+        */
+
+
+        // save cookie for reload.
+        // console.log(messages[0]);
+        // console.log(messages[0].room);
+        // console.log(messages[0].uname);
+
+        // console.log(chatting.uname+' : '+chatting.msg);
+        
+      });
     });
   });
 
@@ -74,15 +108,11 @@ io.on('connection', (socket) => {
     db.query('insert into test(room, uname, msg) values(?, ?, ?)' // add time
     ,[Roomcode, name, msg]
     , function(err, rows, fields) {
-      if (!err)
-        console.log(rows);
-      else
-        console.log('Error while performing Query.', err);
+      // do something
     });
     io.to(Roomcode).emit('chat message', name, msg);
   });
 });
-
 
 http.listen(3000, () => {
   console.log('Connect at 3000');
